5be1d771781bee8ca4cb3fc12adb7622a9a6535d
forsakeljk <forsakeljk@gmail.com>
2024-10-28 21:13:24 +0100

    fix cors

--- src/main/java/com/sep/controller/HomeController.java
+++ src/main/java/com/sep/controller/HomeController.java
@@ -4,15 +4,10 @@ import com.sep.request.LoginRequest;
 import com.sep.response.LoginResponse;
 import com.sep.service.LoginService;
 import jakarta.annotation.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/sep")
 public class HomeController {
@@ -23,17 +18,6 @@ public class HomeController {
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> homePage(@RequestBody LoginRequest req) {
         LoginResponse response = loginService.login(req.getUsername(), req.getPassword());
-        return buildHttpResp(response, HttpStatus.OK);
-    }
-
-    private <T> ResponseEntity<T> buildHttpResp(T res, HttpStatus httpStatus) {
-        MultiValueMap<String, String> headers = new HttpHeaders();
-        headers.add("Access-Control-Allow-Origin", "http://localhost:3000");
-        return new ResponseEntity<>(res, headers, httpStatus);
-    }
-
-    private <T> ResponseEntity<T> buildHttpResp(T res, MultiValueMap<String, String> headers, HttpStatus httpStatus) {
-        headers.add("Access-Control-Allow-Origin", "http://localhost:3000");
-        return new ResponseEntity<>(res, headers, httpStatus);
+        return ResponseEntity.ok(response);
     }
 }
